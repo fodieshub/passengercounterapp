09af5239b6f3769011061b2d089bf7cbf29535d9
fodieshub <buzkenny@gmail.com>
2021-07-11 11:56:19 -0700

    DOM manipulation using JavaScript
    I listened for click event on the two buttons and I wrote two functions
    
    I used get elementById to get the ids of the buttons in the HTML doc
    
    first method is the increment(), count set to 0 is a global variable then in the increment function count is incremented by 1 then count is printed. Secondly, the save()is printing the values in the increment button out and setting the counter to zero and increment button is also set to zero every time the save() function is called

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+
+
+let countEl = document.getElementById('count-el');
+let saveEl = document.getElementById("save-el");
+
+let count = 0;
+
+function increment() {
+    count = count += 1;
+    countEl.textContent = count;
+}
+
+function save() {
+    let countStr = " " + count + " - "
+    saveEl.textContent += countStr;
+    countEl.textContent = 0;
+    count = 0;
+}
